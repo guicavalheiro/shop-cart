3c5eab50e109dac726b8f979f05cb516fce8adf7
guicavalheiro <cavalheiro.dev@gmail.com>
2020-11-12 23:21:55 -0300

    first commit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from carrinho import Carrinho
-import produto
+from produto import Produto
 
 def main():
     teste  = Carrinho.get_instance(Carrinho, "Im test")
@@ -7,7 +7,7 @@ def main():
     teste.adicionar_produto("Vibrador", 2)
     print(teste.dict_produto)
     print()
-        
+
     teste_2 = Carrinho.get_instance(Carrinho, "Vai se fuder")
     print(teste_2.dict_produto)
 
